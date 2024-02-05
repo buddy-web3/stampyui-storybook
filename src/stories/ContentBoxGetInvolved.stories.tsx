b22c8d6261c1a9b649564f4686937e8946d303da
Buddy <0buddy.ne@gmail.com>
2024-02-05 16:23:19 +0200

    v.0.9.1

--- src/stories/ContentBoxGetInvolved.stories.tsx
+++ src/stories/ContentBoxGetInvolved.stories.tsx
@@ -2,18 +2,17 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { ContentBoxGetInvolved } from "../components/ContentBoxGetInvolved/Box";
 
 const meta = {
-    title: "Components/ContentBoxGetInvolved",
-    component: ContentBoxGetInvolved,
-    parameters: {
-        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-        layout: "centered",
-    },
-    tags: ["autodocs"],
+  title: "Components/ContentBoxGetInvolved",
+  component: ContentBoxGetInvolved,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  tags: ["autodocs"],
 } satisfies Meta<typeof ContentBoxGetInvolved>;
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-    args: {
-    },
-}
+  args: {},
+};
